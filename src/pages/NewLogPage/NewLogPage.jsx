e11650ad79440ff950a389fba1152a6aeb1fc37c
sbabaki <sbabaki001@gmail.com>
2024-11-25 14:20:24 -0800

    new log form page v1 complete

--- src/pages/NewLogPage/NewLogPage.jsx
+++ src/pages/NewLogPage/NewLogPage.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from "react";
+import "./NewLogPage.scss"
+import DatePicker from "react-datepicker";
+
 
 export default function NewLogPage() {
   return (
-    <div>NewLogPage</div>
-  )
+    <div className="new-log">
+      <h1 className="new-log__title">New Log</h1>
+      <form className="new-log__form" action="submit">
+        <div className="new-log__label-field">
+          <div className="new-log__label-container">
+            <label htmlFor="new-log__label">Log Name:</label>
+          </div>
+          <div className="new-log__input-container">
+          <input className="new-log__input" type="text" />
+        </div>
+        </div>
+        <div className="new-log__label-field">
+          <div className="new-log__label-container">
+          <label htmlFor="new-log__label">Start Date:</label>
+          </div>
+          <div className="new-log__input-container">
+          <input className="new-log__input" type="text" />
+          </div>
+        </div>
+        <div className="new-log__label-field">
+          <div className="new-log__label-container">
+            <label htmlFor="new-log__label">End Date:</label>
+          </div>
+          <div className="new-log__input-container">
+          <input className="new-log__input" type="text" />
+          </div>
+        </div>
+        <div className="new-log__action-btns">
+        <button type="submit" className="new-log__btn">
+          Submit Log
+        </button>
+        <button type="submit" className="new-log__btn--cancel">
+          Cancel
+        </button>
+        </div>
+      </form>
+    </div>
+  );
 }
